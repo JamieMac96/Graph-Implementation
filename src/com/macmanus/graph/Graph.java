028f1618bacb8fbf2f0a912c18069bb4a124fb34
JamieMac96 <jamiemac96@hotmail.com>
2017-10-11 22:10:55 +0100

    initial commit

---
+++ src/com/macmanus/graph/Graph.java
@@ -0,0 +1,13 @@
+package com.macmanus.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.macmanus.linkedlist.LinkedList;
+
+public abstract class Graph<T> {
+    private List<LinkedList<Integer>> adjacencyList;
+    private List<T> data;
+
+    
+}
